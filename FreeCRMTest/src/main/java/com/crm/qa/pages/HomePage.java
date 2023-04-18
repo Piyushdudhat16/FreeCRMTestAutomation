13e2bed71ed18ad0a20595481e96b1640ecdd64f
Piyushdudhat16 <impiyushdudhat@gmail.com>
2023-04-18 13:24:56 -0400

    Small change

--- FreeCRMTest/src/main/java/com/crm/qa/pages/HomePage.java
+++ FreeCRMTest/src/main/java/com/crm/qa/pages/HomePage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+// import statements
 import com.crm.qa.base.TestBase;
 
 public class HomePage extends TestBase{
